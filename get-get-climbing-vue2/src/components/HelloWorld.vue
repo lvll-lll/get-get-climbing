02f599e6feb4ba360c92435c00e9ac140bf6f860
Your Name <you@example.com>
2021-06-29 14:25:00 +0800

    add files new

---
+++ get-get-climbing-vue2/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li v-for="item in list" :key="item.value" @click="handleClick(item)">
+        <a
+          target="_blank"
+        >
+          {{item.label}}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      list: [
+        { label: 'baidu', value: 'baidu', href: 'http://www.baidu.com' }
+      ]
+    }
+  },
+  methods: {
+    handleClick (item) {
+      console.log(item)
+      this.$router.push('/webglFirst')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ get-get-climbing-vue2/src/views/webgl/webglFirst.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- <div>webgl first page</div> -->
+    <canvas id="c"></canvas>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.drawCanvas()
+  },
+  methods: {
+    drawCanvas () {
+      let canvas = document.querySelector('#c')
+      let gl = canvas.getContext('webgl')
+      if (!gl) {
+        console.log('you do not use webgl')
+      }
+      console.log('start use webgl')
+    }
+  }
+}
+</script>
