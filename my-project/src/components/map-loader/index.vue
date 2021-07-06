f86f47b0559ad1e3373764f9e6c39a0177a11110
lzugis15 <lzuniujp08@hotmail.com>
2021-07-06 20:58:24 +0800

    buildings.json

--- my-project/src/components/map-loader/index.vue
+++ my-project/src/components/map-loader/index.vue
@@ -64,7 +64,7 @@ export default {
     addBuilds () {
       this.map.addSource('buildings', {
         type: 'geojson',
-        data: buildsData
+        data: 'static/data/buildings.json'
       })
       this.map.addLayer({
         'id': '3d-buildings',
